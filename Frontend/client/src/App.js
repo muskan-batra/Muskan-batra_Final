ca1e381a0f2278a9ddbe6806c54b998efb6d5538
=muskan-batra <68785899+muskan-batra@users.noreply.github.com>
2022-07-19 16:13:25 +0500

    Muskan

---
+++ Frontend/client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Header from "./components/header";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/client/src/components/section.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const section = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("token") != undefined) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  return (
+    <div>
+      <nav
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-around",
+          paddingTop: "20px",
+          paddingBottom: "20px",
+          backgroundColor: "lightcyan",
+        }}
+      >
+        {isLoggedIn && <Link to="/">Home</Link>}
+        {/* {!isLoggedIn && <Link to="/login">Login</Link>}
+        {!isLoggedIn && <Link to="/register">Register</Link>}
+        {isLoggedIn && <Link to="/addBook">Add Book</Link>}
+        {isLoggedIn && <Link to="/viewBooks">View Books</Link>}
+        {isLoggedIn && <button type="button">logout</button>} */}
+      </nav>
+    </div>
+  );
+};
+
+export default section;
